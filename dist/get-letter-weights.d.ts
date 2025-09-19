228740163d4459e5f934697867b5b7f862093e28
Dennis Marchand <jacquesporveau@gmail.com>
2025-09-19 15:46:02 -0700

    Remove old dist files

--- dist/get-letter-weights.d.ts
+++
@@ -1,4 +0,0 @@
-export declare const getLetterWeights: ({ words, }: {
-    words: string[];
-}) => Record<string, number>;
-//# sourceMappingURL=get-letter-weights.d.ts.map
--- dist/get-letter-weights.js
+++
@@ -1,9 +0,0 @@
-export const getLetterWeights = ({ words, }) => {
-    return words.reduce((acc, word) => {
-        word.split("").forEach((letter) => {
-            acc[letter] = (acc[letter] || 0) + 1;
-        });
-        return acc;
-    }, {});
-};
-//# sourceMappingURL=get-letter-weights.js.map
